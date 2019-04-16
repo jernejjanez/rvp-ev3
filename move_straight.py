a9f0990a5ed29f98867f6ec5b410d9f96232bf58
Jan Šubelj <jan.subelj010@gmail.com>
2019-04-16 16:12:03 +0200

    dodal pid pa move_straight

---
+++ move_straight.py
@@ -0,0 +1,74 @@
+from pid import PID
+import math
+import time
+class MoveStraight:
+    def __init__(self,tank_pair, gyro):
+        self.tank_pair = tank_pair
+        self.pid_left = PID(100,10,10, max_val=10, min_val=-10)
+        self.pid_right = PID(100,10,10, max_val=10, min_val=-10)
+        self.pid_straight = PID(1000,100,10, max_val=90, min_val=-90)
+        
+        self.time_period = 0.01 # s
+        self.gyro = gyro
+        
+
+        # r = 1.84 cm alpa (kolo) hitrost 1560 °/s
+        # r = 2.744 cm hitrost 1050 °/s
+        # TODO: prevert te cifre
+        self.r = 1.84 #polmer
+        self.speed_deg_per_s = 1560 # °/s
+        
+
+
+    def _cm_moved(self,speed_percent):
+        speed_deg_per_s_current = self.speed_deg_per_s*(speed_percent/100)
+        speed_cm_per_s = (speed_deg_per_s_current/360)*2*math.pi*self.r
+        return speed_cm_per_s*self.time_period
+
+
+    def __call__(self,centimeters):
+        starting_angle = self.gyro.angle
+        curr_path = 0
+        while 1:
+            if (abs(curr_path - centimeters)<0.2):
+                self.pid_left.reset()
+                self.pid_right.reset()
+                self.pid_straight.reset()
+                
+                self.tank_pair.off(brake=True)
+                break
+
+            # Pid for straight path
+            speed_straight = self.pid_straight(centimeters-curr_path)
+            print("error", centimeters-curr_path)
+            print("regval",speed_straight)
+            
+            # rotation pid
+            # TODO: prevert da so napake pravilne
+            # Note: napake so različno računane je gledano na kot aktuatorja
+            angle_reduction_left = self.pid_left(starting_angle - self.gyro.angle)
+            angle_reduction_right = self.pid_right(self.gyro.angle - starting_angle)
+            print(f"angle_left {angle_reduction_left}, angle_right {angle_reduction_right}")
+
+
+            self.tank_pair.on(left_speed=speed_straight+angle_reduction_left, right_speed=speed_straight+angle_reduction_right)
+            curr_path += self._cm_moved(speed_straight)
+            print("Curr_path",curr_path)
+            time.sleep(self.time_period)
+
+
+
+
+class tp_dummy:
+    def on(self, **kwargs):
+        pass
+    def off(self, **kwargs):
+        pass
+
+class gyro_dummy:
+    angle=0
+
+if __name__=="__main__":
+    tp = tp_dummy()
+    move_straight = MoveStraight(tp,gyro_dummy())
+    move_straight(20)
---
+++ pid.py
@@ -0,0 +1,70 @@
+import time
+import math
+
+class PID:
+    def __init__(self, Kp, Ki, Kd, bias=0, max_val=math.inf, min_val=-math.inf):
+        self.error_old = 0
+        self.integral = 0
+        self.time_prev = 0
+
+        self.Kp = Kp
+        self.Ki = Ki
+        self.Kd = Kd
+        
+        self.bias = bias
+        self.max_val = max_val
+        self.min_val = min_val
+
+    def __call__(self, error):
+        current_time = time.time()
+        iteration_time = current_time - self.time_prev
+
+        if self.time_prev != 0:
+            self.integral += (error*iteration_time)
+            if self.integral > self.max_val:
+                self.integral = self.max_val
+            elif self.integral < self.min_val:
+                self.integral = self.min_val
+            derivative = (error - self.error_old)/iteration_time
+            control = self.Kp*error + self.Ki*self.integral + self.Kd*derivative + self.bias
+        else:
+            control = self.Kp*error
+        self.error_old = error
+        self.time_prev = current_time
+
+        if control > self.max_val:
+            control = self.max_val
+        elif control < self.min_val:
+            control = self.min_val
+        
+        return control
+    
+    def reset(self):
+        self.error_old = 0
+        self.integral = 0
+        self.time_prev = 0
+
+def _bell_curve(i):
+    a = [0,0,0,1,2,3,4,5,6,7,7,7,8,8,8,8,8,7,6,5,4,3,2,1,0,0]
+    if i >= len(a):
+        return 0
+    else:
+        return a[i]
+
+if __name__ == "__main__":
+    import random
+    pid = PID(100,10,10)
+    val = 5
+    wanted_val = 0
+    iter = 20
+    i=0
+    while 1:
+        print("err",_bell_curve(i))
+        control = pid(-_bell_curve(i))
+        print("control",control)
+        r = random.random() if random.random() > 0.5 else 0
+        val = val + control + r
+        #print("val",val)
+        time.sleep(0.5)
+        i+=1
+        #pid.reset()
